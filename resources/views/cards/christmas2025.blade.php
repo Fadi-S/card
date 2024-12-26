53b6967533231c1a87b519034d9e051d8bb5613a
Fadi Sarwat <fadisarwat@yahoo.com>
2024-12-26 23:08:53 +0200

    Start adding design elements

--- resources/views/cards/christmas2025.blade.php
+++ resources/views/cards/christmas2025.blade.php
@@ -5,9 +5,31 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Christmas 2025</title>
+    <title>Merry Christmas {{ $name }}</title>
+    @vite('resources/css/app.css')
 </head>
-<body>
-{{ $name }}
+<style>
+    .background {
+        background-image: url('{{ asset("images/top_decor.png") }}');
+        background-size: 28rem auto;
+        background-repeat: no-repeat;
+        background-position: center top;
+    }
+</style>
+
+<body class="bg-orange-100 background">
+
+{{--<img class="" src="{{ asset("images/top_decor.png") }}" alt="Decor" />--}}
+
+<div class="pt-48 p-4 h-screen max-w-xl mx-auto">
+    <div class="border-4 p-3 border-red-600 rounded-lg h-full" dir="rtl">
+        <h1 class="text-2xl font-bold">
+            الابن المبارك/ {{ $name }}
+        </h1>
+
+        <div class="">
+        </div>
+    </div>
+</div>
 </body>
 </html>
