1d1abaf32f276d06011b7c6a0e9b80b3dd812dfe
Fadi Sarwat <fadisarwat@yahoo.com>
2024-12-26 02:48:07 +0200

    Change sign method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ShowCardController;
+use Illuminate\Routing\Middleware\ValidateSignature;
 use Illuminate\Support\Facades\Route;
 
-Route::get("/{card}/{data}", ShowCardController::class)->middleware("signed")->name("preview");
+Route::get("/{card}/{data}", ShowCardController::class)
+    ->middleware(ValidateSignature::class)
+    ->name("preview");
